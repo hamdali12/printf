df641e7dc65087ea1afabd4d5ad99556df042495
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-16 17:24:20 +0100

    Create 1-printf.c

---
+++ 1-printf.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str)
+				{
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return count;
+}
