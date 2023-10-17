7b04f53253cbbcc8a39cc9741f36629950cb4e10
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-17 18:09:32 +0100

    Update and rename printf.c to _printf.c

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && *format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++;
+			count += handle_specifier(&format, &args);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
