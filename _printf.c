28d7217f476809e164745141083f24b16871860d
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-17 19:37:23 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,58 +1,112 @@
-#include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdarg.h>
 
 /**
- * _printf - Custom printf function
- * @format: The format string
+ * _printf - Produces output according to a format.
+ * @format: A character string containing format specifiers.
  *
- * Return: Number of characters printed (excluding null byte)
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
+    int printed_chars = 0;
     va_list args;
-    int count = 0;
 
     va_start(args, format);
 
-    while (format && *format)
+    while (*format)
     {
-        if (*format == '%')
+        if (*format != '%')
         {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str)
-                {
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
+            putchar(*format);
+            printed_chars++;
         }
         else
         {
-            putchar(*format);
-            count++;
+            format++;
+            if (*format == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
+
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    printed_chars++;
+                    break;
+                case 's':
+                    printed_chars += print_string(va_arg(args, char *));
+                    break;
+                case '%':
+                    putchar('%');
+                    printed_chars++;
+                    break;
+                case 'd':
+                case 'i':
+                    printed_chars += print_int(va_arg(args, int));
+                    break;
+                default:
+                    va_end(args);
+                    return (-1);
+            }
         }
+
         format++;
     }
 
     va_end(args);
-    return count;
+    return (printed_chars);
+}
+
+/**
+ * print_string - Prints a string.
+ * @str: The string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(const char *str)
+{
+    int printed_chars = 0;
+
+    if (str == NULL)
+        str = "(null)";
+
+    while (*str)
+    {
+        putchar(*str);
+        printed_chars++;
+        str++;
+    }
+
+    return (printed_chars);
+}
+
+/**
+ * print_int - Prints an integer.
+ * @num: The integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_int(int num)
+{
+    if (num < 0)
+    {
+        putchar('-');
+        num = -num;
+    }
+
+    int printed_chars = 0;
+    if (num == 0)
+    {
+        putchar('0');
+        return (1);
+    }
+    if (num / 10)
+    {
+        printed_chars += print_int(num / 10);
+    }
+    putchar(num % 10 + '0');
+    return (printed_chars + 1);
 }
