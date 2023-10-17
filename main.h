2a13152eae7d8b696c426eabc2d5fd9089f4850e
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-17 18:00:21 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,5 +4,9 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int printf_c(va_list *args);
+int printf_s(va_list *args);
+int printf_i_d(va_list *args);
+int _putchar(char c)
 
 #endif /* _MAIN_H */
