78974374f523d30c94391b2cc997182bb71ca793
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-17 18:17:28 +0100

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Write a character to stdout
+ * @c: The character to print
+ *
+ * Return: The number of characters printed
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
