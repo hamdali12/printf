4a57f67d8ee8ff7e76e8d8cda6aee0395c27c094
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-10-17 16:41:58 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,97 +1,57 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int char_count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && *format)
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            char_count++;
-        }
-        else
-        {
-            format++; // Move past '%'
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char c = va_arg(args, int);
-                    putchar(c);
-                    char_count++;
-                    break;
-                }
-                case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(null)";
-                    while (*str)
-                    {
-                        putchar(*str);
-                        char_count++;
-                        str++;
-                    }
-                    break;
-                }
-                case 'd':
-                case 'i':
-                {
-                    int num = va_arg(args, int);
-                    if (num < 0)
-                    {
-                        putchar('-');
-                        char_count++;
-                        num = -num;
-                    }
-                    int digit_count = 0;
-                    int temp = num;
-                    while (temp > 0)
-                    {
-                        temp /= 10;
-                        digit_count++;
-                    }
-                    int divisor = 1;
-                    for (int i = 1; i < digit_count; i++)
-                        divisor *= 10;
-                    while (divisor > 0)
-                    {
-                        char digit = (num / divisor) + '0';
-                        putchar(digit);
-                        char_count++;
-                        num %= divisor;
-                        divisor /= 10;
-                    }
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    char_count++;
-                    break;
-                }
-                default:
-                {
-                    putchar('%');
-                    char_count++;
-                    if (*format != '\0')
-                    {
-                        putchar(*format);
-                        char_count++;
-                    }
-                }
-            }
-        }
-        format++;
-    }
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str)
+				{
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-    va_end(args);
-    return char_count;
+	va_end(args);
+	return count;
 }
